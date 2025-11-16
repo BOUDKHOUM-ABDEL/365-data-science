64fd134053fb5eaa70773e9af5507f4cdd5a3ac4
BOUDKHOUM-ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-11-16 18:58:18 +0100

    update

---
+++ Data Preprocessing with NumPy/test.py
@@ -0,0 +1,41 @@
+# Write a Python script that uses NumPy to create a 1D array and a 2D array (matrix).
+ 
+# The 1D array should contain all even integers from 1 to 9 in ascending order.
+# The matrix should have 2 rows with the following values:
+# Row 1: 0, 1, 0
+# Row 2: 1, 0, 1  
+ 
+# Finally, print the result in the following format:
+# First, print the text "1D Array:" on one line, followed by the 1D array on the next line.
+# Then, print an empty line ("\n") to separate the outputs.
+# After the empty line, print the text "2D Matrix:" followed by the matrix on the next line
+import numpy as np
+
+# 1D array of all even integers
+arr_1d = np._______
+
+# 2D matrix 2x3
+arr_2d = ________
+
+# Print the array and matrix
+print("1D Array:")
+print(arr_1d)
+
+print("\n2D Matrix:")
+print(arr_2d)
+
+
+import numpy as np
+
+# 1D array of all even integers
+arr_1d = np.array([0,1,0])
+
+# 2D matrix 2x3
+arr_2d = np.array([[1,2],[3,4],[5,6]])
+
+# Print the array and matrix
+print("1D Array:")
+print(arr_1d)
+
+print("\n2D Matrix:")
+print(arr_2d)
